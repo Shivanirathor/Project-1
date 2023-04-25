b48e2866ad9788ba69984f345b99890ee47a7ed2
Shivanirathor <Shivanirathor034@gmail.com>
2023-04-26 01:17:06 +0530

    text

--- src/practice/text.js
+++ src/practice/text.js
@@ -14,7 +14,6 @@ let obj = [
 },
 ];
 obj.sort((a,b)=>{
-    
   if(a.name < b.name){
     return -1;
   }
@@ -27,7 +26,51 @@ obj.sort((a,b)=>{
 console.log(obj);
 
 
-// let b = ["mika", "alia", "ali", "shivani"]
+// let b = []
 // console.log(b.sort())
+
+
+
+
+
+// =======================
+// sort, filter, reduce, map, includes
+
+// let arr1 = ["mika", "alia", "ali", "shivani"]
+// console.log(arr1.sort());
+// const map = arr1.map((x)=>{
+//   return x+123;
+// })
+// console.log(map)
+// let a = [3,2,3,2,3]
+// const result = a.filter((x)=>{
+//    return x - 2; 
+// })
+// console.log(result)
+
+// let b = [10,2,33,20,3]
+// const map = b.map((x)=>{
+//   return x*2;
+// });
+// console.log(map);
+
+
+
+// let arr = ["mika", "alia", "ali", "shivani"]
+// let givenName = "ali12";
+
+// console.log(arr.includes((givenName)));
+
+
+
+// =======
+const number = [1,2,"Afroj","shivani","shaily","harish","vishal",5,10];
+
+// const
+const numbers = [1,2,3,4,5,6];
+const sumnumbers = numbers.reduce((total, item) => total + item, 3); //hello is initial value
+console.log(sumnumbers);
+
+
 
 
